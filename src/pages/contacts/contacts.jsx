66b41e318ca55348d475eaec9fed922f1e32be64
Mark Korobkin <markkorobkin85@gmail.com>
2025-09-05 00:34:01 +0300

    tiny little title tag error

--- src/pages/contacts/contacts.jsx
+++ src/pages/contacts/contacts.jsx
@@ -6,7 +6,7 @@ function Contacts() {
 
     return (
         <div className="contact">
-            <title>Contact us</title>
+            <title>Contacts</title>
 			<p class="contacts-title">Contacts</p>
 			<div class="contacts-icons">
 				<a href="https://www.facebook.com/profile.php?id=100034058891404"><img src={facebookLogo} alt="facebook" class="contacts-icon" /></a>
